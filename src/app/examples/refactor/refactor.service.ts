e56b818bf56e76850ec8bc94f1d636b9457a0523
Lukas Ruebbelke <simpul@gmail.com>
2018-07-14 21:34:39 -0700

    update: adding refactor service

---
+++ src/app/examples/refactor/refactor.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { UUID } from 'angular2-uuid';
+import { Widget, WidgetsService } from '../../shared';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RefactorService {
+  price;
+  mode;
+  widgets: Widget[];
+
+  constructor() {
+  }
+
+  reCalculateTotal(mode, widgets, newWidget) {
+    this.widgets = this.updateWidgets(mode, widgets, newWidget);
+    this.price = this.getTotalPrice(this.widgets);
+  }
+
+  updateWidgets(mode, widgets, newWidget) {
+    switch (mode) {
+      case 'create':
+        return this.addWidget(widgets, newWidget);
+      case 'update':
+        return this.updateWidget(widgets, newWidget);
+      case 'delete':
+        return this.deleteWidget(widgets, newWidget);
+      default:
+        return widgets;
+    }
+  }
+
+  addWidget(widgets, widget) {
+    const newWidget = Object.assign({}, widget, {id: UUID.UUID()});
+    return [...widgets, newWidget];
+  }
+
+  updateWidget(widgets, widget) {
+    return widgets.map(wdgt => (widget.id === wdgt.id) ? Object.assign({}, widget) : wdgt);
+  }
+
+  deleteWidget(widgets, widget) {
+    return widgets.filter(wdgt => widget.id !== wdgt.id);
+  }
+
+  getTotalPrice(widgets) {
+    return widgets.reduce((acc, curr) => acc + curr.price, 0);
+  }
+}
