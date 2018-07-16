ea1f119bb15bf37e1f7b9324e6b5f4a1116e7d97
Lukas Ruebbelke <simpul@gmail.com>
2018-07-15 17:32:00 -0700

    update: adding test to detail components

--- src/app/items/item-detail/item-detail.component.spec.ts
+++ src/app/items/item-detail/item-detail.component.spec.ts
@@ -1,11 +1,75 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
-import { ItemDetailComponent } from './item-detail.component';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { AppMaterialModule } from '../../app-material.module';
 
-describe('Component: ItemDetail', () => {
-  it('should create an instance', () => {
-    const component = new ItemDetailComponent();
+import { ItemDetailComponent } from './item-detail.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { Item } from '../../shared';
+
+describe('ItemDetailComponent', () => {
+  let fixture: ComponentFixture<ItemDetailComponent>;
+  let de: DebugElement;
+  let component: ItemDetailComponent;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ItemDetailComponent],
+      imports: [FormsModule, BrowserAnimationsModule, AppMaterialModule]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ItemDetailComponent);
+    component = fixture.componentInstance;
+    de = fixture.debugElement;
+    component.item = {id: 100, name: 'item', description: 'item', price: 100};
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the originalName', () => {
+    component.item = {id: 100, name: 'UPDATED NAME', description: 'item', price: 100};
+    expect(component.originalName).toBe('UPDATED NAME');
+  });
+
+  it('should set selectedItem when item is updated', () => {
+    const newItem = {id: 200, name: 'NAME', description: 'DESC', price: 1000};
+    component.item = newItem;
+    expect(component.selectedItem).toEqual(newItem);
+  });
+
+  it('should raise a `cancelled` event on cancelled button click', () => {
+    component.item = {id: 200, name: 'NAME', description: 'DESC', price: 1000};
+
+    const cancelButton = de.query(By.css('button[type="button"]'));
+    let cancelledItem: Item;
+
+    component.cancelled
+      .subscribe((item: Item) => cancelledItem = item);
+
+    cancelButton.triggerEventHandler('click', null);
+    expect(component.selectedItem).toBe(cancelledItem);
+  });
+
+  it('should raise a `saved` event after form submitted', () => {
+    component.item = {id: 200, name: 'NAME', description: 'DESC', price: 1000};
+
+    const form = de.query(By.css('form'));
+    let savedItem: Item;
+
+    component.saved
+      .subscribe((item: Item) => savedItem = item);
+
+    form.triggerEventHandler('submit', null);
+    expect(component.selectedItem).toBe(savedItem);
   });
 });
--- src/app/widgets/widget-detail/widget-detail.component.spec.ts
+++ src/app/widgets/widget-detail/widget-detail.component.spec.ts
@@ -1,3 +1,5 @@
+/* tslint:disable:no-unused-variable */
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
@@ -5,7 +7,8 @@ import { AppMaterialModule } from '../../app-material.module';
 
 import { WidgetDetailComponent } from './widget-detail.component';
 import { DebugElement } from '@angular/core';
-import { WidgetsListComponent } from '../widgets-list/widgets-list.component';
+import { By } from '@angular/platform-browser';
+import { Widget } from '../../shared';
 
 describe('WidgetDetailComponent', () => {
   let fixture: ComponentFixture<WidgetDetailComponent>;
@@ -13,10 +16,10 @@ describe('WidgetDetailComponent', () => {
   let component: WidgetDetailComponent;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [WidgetDetailComponent],
-      imports: [FormsModule, BrowserAnimationsModule, AppMaterialModule]
-    })
+      TestBed.configureTestingModule({
+        declarations: [WidgetDetailComponent],
+        imports: [FormsModule, BrowserAnimationsModule, AppMaterialModule]
+      })
       .compileComponents();
   }));
 
@@ -24,7 +27,6 @@ describe('WidgetDetailComponent', () => {
     fixture = TestBed.createComponent(WidgetDetailComponent);
     component = fixture.componentInstance;
     de = fixture.debugElement;
-
     component.widget = {id: 100, name: 'item', description: 'item', price: 100};
 
     fixture.detectChanges();
@@ -32,5 +34,42 @@ describe('WidgetDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the originalName', () => {
+    component.widget = {id: 100, name: 'UPDATED NAME', description: 'item', price: 100};
+    expect(component.originalName).toBe('UPDATED NAME');
+  });
+
+  it('should set selectedWidget when widget is updated', () => {
+    const newWidget = {id: 200, name: 'NAME', description: 'DESC', price: 1000};
+    component.widget = newWidget;
+    expect(component.selectedWidget).toEqual(newWidget);
+  });
+
+  it('should raise a `cancelled` event on cancelled button click', () => {
+    component.widget = {id: 200, name: 'NAME', description: 'DESC', price: 1000};
+
+    const cancelButton = de.query(By.css('button[type="button"]'));
+    let cancelledWidget: Widget;
+
+    component.cancelled
+      .subscribe((widget: Widget) => cancelledWidget = widget);
+
+    cancelButton.triggerEventHandler('click', null);
+    expect(component.selectedWidget).toBe(cancelledWidget);
+  });
+
+  it('should raise a `saved` event after form submitted', () => {
+    component.widget = {id: 200, name: 'NAME', description: 'DESC', price: 1000};
+
+    const form = de.query(By.css('form'));
+    let savedWidget: Widget;
+
+    component.saved
+      .subscribe((widget: Widget) => savedWidget = widget);
+
+    form.triggerEventHandler('submit', null);
+    expect(component.selectedWidget).toBe(savedWidget);
   });
 });
