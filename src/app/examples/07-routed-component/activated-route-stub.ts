93d2fc2185f238d4d4080c3e0fac6c4e5d02d5cb
Lukas Ruebbelke <simpul@gmail.com>
2018-07-16 05:30:52 -0700

    update: removing activated route stub

--- src/app/examples/07-routed-component/activated-route-stub.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-
-@Injectable()
-export class ActivatedRouteStub {
-  private subject = new BehaviorSubject({subject: 'planet'});
-  params = this.subject.asObservable();
-}
